70db92fe6a35a0cb1ec01ba5f69ea2675842fd7e
Nichodon <brandonsychung@gmail.com>
2018-08-06 20:02:04 -0700

    add parallax

--- chowning/main.js
+++ chowning/main.js
@@ -4,6 +4,7 @@ document.addEventListener('scroll', function() {
 
 function update() {
     var percent = document.documentElement.scrollTop / document.documentElement.scrollHeight;
+    document.getElementById('image').style.marginTop = (2050 * percent) + 'px';
     document.getElementById('back').style.backgroundPosition = '0 ' + (50 * percent) + '%';
 }
 
