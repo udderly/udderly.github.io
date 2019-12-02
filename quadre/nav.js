5745283f0f526b4d506e57248e0c8d6d1993729f
Nichodon <brandonsychung@gmail.com>
2019-12-01 21:26:39 -0800

    make tour responsive

--- quadre/nav.js
+++ quadre/nav.js
@@ -46,7 +46,7 @@ document.write(`
     <div id="footer">
         <p style="margin-top: 10px">&copy; 2019 Quadre</p>
         <p>Photography by <a href="https://www.carlinmaphotography.com/" target="_blank">Carlin Ma</a></p>
-        <p style="margin-bottom: -20px">Website by <a href="https://udderly.github.io/" target="_blank">Brandon Chung</a></p>
+        <p style="margin-bottom: -10px">Website by <a href="https://udderly.github.io/" target="_blank">Brandon Chung</a></p>
         <button onclick="window.location.href = '${PATH}connect/'">Book</button>
         <button onclick="window.location.href = '${PATH}support/'">Donate</button>
     </div>
