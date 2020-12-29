2c562ebcda2278beb300dc92addce58c614c32b6
Nichodon <brandonsychung@gmail.com>
2020-12-29 10:47:38 -0800

    do new changes

--- quadre/nav.js
+++ quadre/nav.js
@@ -63,7 +63,7 @@ document.write(`
     </div>
         
     <div id="popup">
-        <p id="popuptext">Our album &ldquo;Our Time&rdquo; is available <a href="https://store.cdbaby.com/cd/quadrethevoiceoffourhorn" target="_blank">here</a>!</p>
+        <p id="popuptext">Our album &ldquo;Our Time&rdquo; is available <a href="https://quadre.bandcamp.com/album/our-time" target="_blank">here</a>!</p>
     </div>
 
     <div id="footer">
@@ -118,6 +118,16 @@ document.write(`
             <a href="https://www.linkedin.com/company/quadre---the-voice-of-four-horns" target="_blank">
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64.79 64.79" width="32" height="32">
 	<path d="M55.2,55.2H45.6v-15c0-3.58-.06-8.2-5-8.2s-5.76,3.91-5.76,7.94V55.2h-9.6V24.29h9.21v4.22h.13a10.09,10.09,0,0,1,9.09-5c9.73,0,11.53,6.4,11.53,14.72ZM14.41,20.06A5.57,5.57,0,1,1,20,14.49a5.57,5.57,0,0,1-5.57,5.57h0m4.8,35.14H9.6V24.29h9.61ZM60,0H4.78A4.72,4.72,0,0,0,0,4.67V60.11a4.73,4.73,0,0,0,4.78,4.68H60a4.74,4.74,0,0,0,4.8-4.68V4.67A4.75,4.75,0,0,0,60,0" transform="translate(0 0)"/>
+</svg>
+            </a>
+            <a href="https://twitter.com/Quadre4Horns" target="_blank">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 203.14" width="32" height="32">
+    <path class="cls-1" d="M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.24,102.24,0,0,1-29.46,8.07,51.46,51.46,0,0,0,22.55-28.37,102.76,102.76,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.63,145.63,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.84,50.84,0,0,1,85,169.86v.65a51.31,51.31,0,0,0,41.15,50.28,51.2,51.2,0,0,1-23.16.88,51.36,51.36,0,0,0,47.92,35.62A102.9,102.9,0,0,1,75,278.55a145.22,145.22,0,0,0,78.62,23" transform="translate(-75 -98.45)"/>
+</svg>
+            </a>
+            <a href="https://www.youtube.com/channel/UCbqmZxiusrcp8hiYNw_8KKg" target="_blank">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 121.48 85.04" width="32" height="32">
+    <path d="M203.5,98.32a15.23,15.23,0,0,0-10.74-10.74C183.28,85,145.29,85,145.29,85s-38,0-47.46,2.54A15.21,15.21,0,0,0,87.09,98.32c-2.54,9.47-2.54,29.24-2.54,29.24s0,19.77,2.54,29.24a15.21,15.21,0,0,0,10.74,10.74c9.47,2.54,47.46,2.54,47.46,2.54s38,0,47.47-2.54A15.23,15.23,0,0,0,203.5,156.8c2.53-9.47,2.53-29.24,2.53-29.24S206,107.79,203.5,98.32Zm-70.36,47.46V109.34l31.56,18.22Z" transform="translate(-84.55 -85.04)"/>
 </svg>
             </a>
         </div>
